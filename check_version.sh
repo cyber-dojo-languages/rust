1ee4bbc8945d089b35e64d796d2f32e9321c0b9e
Jon Jagger <jon@jaggersoft.com>
2021-01-01 08:40:20 +0000

    Now at 1.49

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=1.48
+readonly EXPECTED=1.49
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'rustc --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
