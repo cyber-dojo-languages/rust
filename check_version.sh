8f7ccdbf6112f18eed43b9386bc3550c3fdff1a2
Faye <faye@kosli.com>
2025-02-13 14:28:20 +0000

    Upgrade to 1.84.1

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED="rustc 1.84.0"
+readonly EXPECTED="rustc 1.84.1"
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'rustc --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
